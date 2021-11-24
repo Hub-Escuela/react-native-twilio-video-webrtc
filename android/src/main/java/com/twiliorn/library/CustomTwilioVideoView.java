43cb4ebd61f146852d60d43d94bebedbfbfc5d12
abarbeitohub <68293445+abarbeitohub@users.noreply.github.com>
2021-11-24 09:39:02 +0100

    Reduce resolution and fps

--- android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
+++ android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
@@ -242,7 +242,7 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
     // ===== SETUP =================================================================================
 
     private VideoFormat buildVideoFormat() {
-        return new VideoFormat(VideoDimensions.HD_720P_VIDEO_DIMENSIONS, 30);
+        return new VideoFormat(VideoDimensions.WVGA_VIDEO_DIMENSIONS, 25);
     }
 
     private CameraCapturer createCameraCaputer(Context context, String cameraId) {
